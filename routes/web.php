32d25f92591b8a22560e4714d2a4351d0cccf084
Nikolay Malchikov <malchikov.ne@gmail.com>
2022-09-21 14:30:06 +0300

    update

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
+Route::get('/', function () {
+    return view('home');
+});
+
 Route::post('/urls', function () {
-    return view('welcome');
+    return view('analyzerPage');
 });
