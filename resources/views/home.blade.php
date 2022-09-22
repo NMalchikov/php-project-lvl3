02e5372b9e34c49124f29facebab5cfb35e3f740
Nikolay Malchikov <malchikov.ne@gmail.com>
2022-09-22 23:42:25 +0300

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,6 @@
 @extends('layout')
-@section('main_content')
-        <main class="flex-grow-1">
-                            <div class="container-lg mt-3">
-        <div class="row">
-            <div class="col-12 col-md-10 col-lg-8 mx-auto border rounded-3 bg-light p-5">
-                <h1 class="display-3">Анализатор страниц</h1>
-                @if($errors->any());
+@section('error')
+                @if($errors->any())
                     <div class="allert alert-danger">
                         <ul>
                             @foreach($errors->all() as $error)
@@ -14,6 +9,13 @@
                         </ul>
                     </div>
                 @endif
+$endsection
+@section('main_content')
+        <main class="flex-grow-1">
+                            <div class="container-lg mt-3">
+        <div class="row">
+            <div class="col-12 col-md-10 col-lg-8 mx-auto border rounded-3 bg-light p-5">
+                <h1 class="display-3">Анализатор страниц</h1>
                 <p class="lead">Бесплатно проверяйте сайты на SEO пригодность</p>
                 <form action="https://lvl3-php.herokuapp.com/urls" method="post" class="d-flex justify-content-center">
                     @csrf                 
