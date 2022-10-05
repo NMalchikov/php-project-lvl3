3cdf360b694f85835bee8ba2a4e61b3b020645e6
Nikolay Malchikov <malchikov.ne@gmail.com>
2022-10-05 14:35:31 +0300

    update

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,9 +4,6 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- CSRF Token -->
-        <meta name="csrf-token" content="lO7PVbnALTEvE52J6okGrHC2nOtuwraD5TUdY1yZ">
-
         <title>Анализатор страниц</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
@@ -31,6 +28,7 @@
             </nav>
         </header>
         <main class="flex-grow-1">
+        @include('flash::message')
         @yield('main_content')
 </main>
    
