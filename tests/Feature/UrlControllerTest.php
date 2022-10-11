6f6a1c1bb00ed07d51d5608deb4a89ec262725eb
Nikolay Malchikov <malchikov.ne@gmail.com>
2022-10-11 20:36:33 +0300

    update

---
+++ tests/Feature/UrlControllerTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Tests\Feature;
+
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+use Tests\TestCase;
+
+class UrlControllerTest extends TestCase
+{
+    private array $data;
+    private int $id;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->data = [
+            'name' => 'https://malchikov.pro',
+            'created_at' => Carbon::now(),
+        ];
+
+        $this->id = DB::table('urls')->insertGetId($this->data);
+    }
+
+    public function testIndex()
+    {
+        $response = $this->get(route('urls.index'));
+        $response->assertOk();
+    }
+
+    public function testStore()
+    {
+        $response = $this->followingRedirects()->post(route('urls.store'), ['url' => $this->data]);
+        $response->assertSessionHasNoErrors();
+        $response->assertOk();
+        $response->assertSeeText($this->data['name']);
+        $this->assertDatabaseHas('urls', $this->data);
+    }
+
+    public function testShow()
+    {
+        $response = $this->get(route('urls.show', ['url' => $this->id]));
+        $response->assertOk();
+        $response->assertSeeText($this->data['name']);
+    }
+}
