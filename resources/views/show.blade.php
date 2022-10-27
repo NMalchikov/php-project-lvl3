80ba34787d4a1805995a826c1be91bca06e5dee6
Nikolay Malchikov <malchikov.ne@gmail.com>
2022-10-27 16:27:56 +0300

    update

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -38,9 +38,9 @@
                     <tr>
                         <td>{{ $check->id }}</td>
                         <td>{{ $check->status_code }}</td>
-                        <td>{{ Str::limit($check->h1, 50) }}</td>
-                        <td>{{ Str::limit($check->title, 50) }}</td>
-                        <td>{{ Str::limit($check->description, 50) }}</td>
+                        <td>{{ Str::limit($check->h1, 30) }}</td>
+                        <td>{{ Str::limit($check->title, 30) }}</td>
+                        <td>{{ Str::limit($check->description, 30) }}</td>
                         <td>{{ $check->created_at }}</td>
                     </tr>
                 @endforeach
